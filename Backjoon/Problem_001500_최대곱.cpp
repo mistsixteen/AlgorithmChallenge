44aaed66a8543483f3bc726b79a17f3f89f9d685
mistsixteen <mist.sixteen@gmail.com>
2022-03-07 18:04:58 +0900

    백준 1500번 문제 풀이 업로드

---
+++ Backjoon/Problem_001500_최대곱.cpp
@@ -0,0 +1,45 @@
+﻿//Backjoon Problem No.1500
+//https://www.acmicpc.net/problem/1500
+//Mist16, 2022-03-07
+
+#include <iostream>
+
+using namespace std;
+
+unsigned long long maxValueArray[101][20] = { 0, };
+
+unsigned long long findMaxValue(int left, int leftidx)
+{
+	unsigned long long maxValue = 0;
+	if (maxValueArray[left][leftidx] != 0)
+		return maxValueArray[left][leftidx];
+	if (left == leftidx)
+	{
+		maxValueArray[left][leftidx] = 1;
+		return 1;
+	}
+	if (leftidx == 1)
+	{
+		maxValueArray[left][leftidx] = left;
+		return left;
+	}
+	for (int i = 1; i < left - leftidx + 1; i++)
+	{
+		unsigned long long temp = i * findMaxValue(left - i, leftidx - 1);
+		if (temp > maxValue)
+			maxValue = temp;
+	}
+	maxValueArray[left][leftidx] = maxValue;
+	return maxValue;
+}
+
+
+int main()
+{
+	int s, k;
+	cin >> s >> k;
+
+	cout << findMaxValue(s, k);
+
+	return 0;
+}
