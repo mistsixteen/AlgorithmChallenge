07c785762752bcc44a9f3c0df923e903d1cec96c
mistsixteen <mist.sixteen@gmail.com>
2022-03-10 21:57:18 +0900

    10250번 문제 풀이 업로드
    10250번 문제 풀이 업로드

---
+++ Backjoon/Problem_010250_ACM호텔.cpp
@@ -0,0 +1,35 @@
+﻿//Backjoon Problem No.10250
+//https://www.acmicpc.net/problem/10250
+//Mist16, 2022-03-10
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int testCase;
+	int H, W, N;
+
+	cin >> testCase;
+
+	for (int i = 0; i < testCase; i++)
+	{
+		int height = 0, width = 1;
+		cin >> H >> W >> N;
+
+		while (N > H)
+		{
+			width++;
+			N = N - H;
+		}
+		height = N;
+		
+		cout << height;
+		cout.width(2);
+		cout.fill('0');
+			cout << width << "\n";
+	}
+
+	return 0;
+}
