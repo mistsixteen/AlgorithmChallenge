7a7d79485da9c12b14c5bde4ec58a05b4c864c7d
mistsixteen <mist.sixteen@gmail.com>
2022-03-11 20:14:27 +0900

    14501번 문제 풀이 업로드
    14501번 문제 풀이 업로드

---
+++ Backjoon/Problem_014501_퇴사.cpp
@@ -0,0 +1,45 @@
+﻿//Backjoon Problem No.14501
+//https://www.acmicpc.net/problem/14501
+//Mist16, 2022-03-11
+
+#include <iostream>
+using namespace std;
+
+static const int MAX_DAYS = 15;
+
+int t[MAX_DAYS] = { 0, }, p[MAX_DAYS] = { 0, };
+int N = 0;
+
+int maxP[MAX_DAYS + 1] = { 0, };
+
+void run(int cDay, int cPrice)
+{
+	if (cDay > N)
+		return;
+	if (cPrice < maxP[cDay])
+		return;
+	maxP[cDay] = cPrice;
+
+	if (cDay == N)
+		return;
+
+	run(cDay + 1, cPrice);
+	run(cDay + t[cDay], cPrice + p[cDay]);
+
+}
+
+int main()
+{
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> t[i] >> p[i];
+	}
+
+	run(0, 0);
+
+	cout << maxP[N];
+	return 0;
+}
