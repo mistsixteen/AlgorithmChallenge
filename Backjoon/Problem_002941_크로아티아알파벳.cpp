2c22f5785dad416585a4e4e95cbefdb7e50814d2
mistsixteen <mist.sixteen@gmail.com>
2022-03-15 13:20:07 +0900

    2941번 문제 풀이 업로드
    2941번 문제 풀이 업로드

---
+++ Backjoon/Problem_002941_크로아티아알파벳.cpp
@@ -0,0 +1,91 @@
+﻿//Backjoon Problem No.2941
+//https://www.acmicpc.net/problem/2941
+//Mist16, 2022-03-15
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+
+	cin >> input;
+	int crtNum = 0;
+
+	for (int i = 0; i < input.size(); i++)
+	{
+		switch (input[i])
+		{
+		case 'c':
+			if (i + 1 < input.size() && (input[i + 1] == '=' || input[i + 1] == '-'))
+			{
+				crtNum++;
+				i++;
+			}
+			else
+				crtNum++;
+			break;
+		case 'd':
+			if (i + 1 < input.size() && input[i + 1] == '-')
+			{
+				crtNum++;
+				i++;
+			}
+			else if (i + 2 < input.size() && input[i + 1] == 'z' && input[i + 2] == '=')
+			{
+				crtNum++;
+				i = i + 2;
+			}
+			else
+				crtNum++;
+			break;
+		case 'l':
+			if (i + 1 < input.size() && input[i + 1] == 'j')
+			{
+				crtNum++;
+				i++;
+			}
+			else
+				crtNum++;
+			break;
+		case 'n':
+			if (i + 1 < input.size() && input[i + 1] == 'j')
+			{
+				crtNum++;
+				i++;
+			}
+			else
+				crtNum++;
+			break;
+		case 's':
+			if (i + 1 < input.size() && input[i + 1] == '=')
+			{
+				crtNum++;
+				i++;
+			}
+			else
+				crtNum++;
+			break;
+		case 'z':
+			if (i + 1 < input.size() && input[i + 1] == '=')
+			{
+				crtNum++;
+				i++;
+			}
+			else
+				crtNum++;
+			break;
+		default:
+			crtNum++;
+		}
+
+	}
+
+	cout << crtNum;
+
+
+
+	return 0;
+}
