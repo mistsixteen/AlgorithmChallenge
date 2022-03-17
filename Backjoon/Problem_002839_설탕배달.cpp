7f90450188c824480f696b7afc223b9146e6c3f4
mistsixteen <mist.sixteen@gmail.com>
2022-03-17 23:24:27 +0900

    2839번 문제 풀이 업로드
    2839번 문제 풀이 업로드

---
+++ Backjoon/Problem_002839_설탕배달.cpp
@@ -0,0 +1,31 @@
+﻿//Backjoon Problem No.2839
+//https://www.acmicpc.net/problem/2839
+//Mist16, 2022-03-17
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Kilograms;
+	int totalBoxes = -1;
+	int boxOf5Kilo;
+
+	cin >> Kilograms;
+
+	boxOf5Kilo = Kilograms / 5;
+
+	for (int i = boxOf5Kilo; i >= 0; i--)
+	{
+		if ((Kilograms - i * 5) % 3 == 0)
+		{
+			totalBoxes = i + ((Kilograms - i * 5) / 3);
+			break;
+		}
+	}
+	cout << totalBoxes;
+
+
+	return 0;
+}
