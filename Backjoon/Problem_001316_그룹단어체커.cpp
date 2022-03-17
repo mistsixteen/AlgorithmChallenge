1ce3c72c1b72ff88278b6b5eebf59705f5e66921
mistsixteen <mist.sixteen@gmail.com>
2022-03-17 23:24:18 +0900

    1316번 문제 풀이 업로드
    1316번 문제 풀이 업로드

---
+++ Backjoon/Problem_001316_그룹단어체커.cpp
@@ -0,0 +1,53 @@
+﻿//Backjoon Problem No.1316
+//https://www.acmicpc.net/problem/1316
+//Mist16, 2022-03-16
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+	int testCases;
+
+	cin >> testCases;
+	int gStrNum = 0;
+
+	for (int i = 0; i < testCases; i++)
+	{
+		cin >> input;
+		bool isGroupString = true;
+		int isUsed[26] = { 0, };
+		char lastChar = ' ';
+
+		for (char c : input)
+		{
+			if (c == lastChar)
+				continue;
+			else
+			{
+				if (isUsed[(int)c - (int)'a'] == 0)
+				{
+					isUsed[(int)c - (int)'a'] = 1;
+					lastChar = c;
+				}
+				else
+				{
+					isGroupString = false;
+					break;
+				}
+			}
+		}
+
+		if (isGroupString == true)
+		{
+			gStrNum++;
+		}	
+	}
+
+	cout << gStrNum;
+
+	return 0;
+}
