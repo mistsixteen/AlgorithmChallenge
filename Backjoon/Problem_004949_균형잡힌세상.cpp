198ea9a8369694717e08f9bce3b698da7b73615b
mistsixteen <mist.sixteen@gmail.com>
2022-03-23 04:27:36 +0900

    4949번, 9012번 문제 풀이 업로드
    4949번, 9012번 문제 풀이 업로드

---
+++ Backjoon/Problem_004949_균형잡힌세상.cpp
@@ -0,0 +1,74 @@
+﻿//Backjoon Problem No.4949
+//https://www.acmicpc.net/problem/4949
+//Mist16, 2022-03-23
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+
+bool run(string testCase)
+{
+	int minicaseLevel = 1;
+	int bigcaseLevel = 1;
+	stack<int> cases;
+
+	for (char c : testCase)
+	{
+		if (c == '(')
+		{
+			cases.push(minicaseLevel + 100);
+			minicaseLevel++;
+		}
+		if (c == ')')
+		{
+			if (cases.empty())
+				return false;
+			minicaseLevel--;
+			if (cases.top() != (minicaseLevel + 100))
+				return false;			
+			cases.pop();
+		}
+		if (c == '[')
+		{
+			cases.push(bigcaseLevel + 200);
+			bigcaseLevel++;
+		}
+		if (c == ']')
+		{
+			if (cases.empty())
+				return false;
+			bigcaseLevel--;
+			if (cases.top() != (bigcaseLevel + 200))
+				return false;
+			cases.pop();
+		}
+	}
+	if (cases.empty())
+		return true;
+	return false;
+}
+
+int main()
+{
+	string tCase;
+	//입출력 속도 증가
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	while(true)
+	{
+		getline(cin, tCase);
+		if (tCase.size() == 1 && tCase[0] == '.')
+			break;
+		if (run(tCase))
+			cout << "yes\n";
+		else
+			cout << "no\n";
+	}
+
+	return 0;
+}
---
+++ Backjoon/Problem_009012_괄호.cpp
@@ -0,0 +1,58 @@
+﻿//Backjoon Problem No.9012
+//https://www.acmicpc.net/problem/9012
+//Mist16, 2022-03-23
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+
+bool run(string testCase)
+{
+	int caseLevel = 1;
+	stack<int> cases;
+
+	for (char c : testCase)
+	{
+		if (c == '(')
+		{
+			cases.push(caseLevel++);
+		}
+		if (c == ')')
+		{
+			if (cases.empty())
+				return false;
+			caseLevel--;
+			if (cases.top() != caseLevel)
+				return false;			
+			cases.pop();
+		}
+	}
+	if (cases.empty())
+		return true;
+	return false;
+}
+
+int main()
+{
+	int numTestCase = 0;
+	string tCase;
+	//입출력 속도 증가
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> numTestCase;
+
+	for (int i = 0; i < numTestCase; i++)
+	{
+		cin >> tCase;
+		if (run(tCase))
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+
+	return 0;
+}
