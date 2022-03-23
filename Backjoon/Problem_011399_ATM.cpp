16340673940a77b5deee7086f16797469a411f7d
mistsixteen <mist.sixteen@gmail.com>
2022-03-24 06:56:05 +0900

    11399번 문제 풀이 업로드
    11399번 문제 풀이 업로드

---
+++ Backjoon/Problem_011399_ATM.cpp
@@ -0,0 +1,38 @@
+﻿//Backjoon Problem No.11399
+//https://www.acmicpc.net/problem/11399
+//Mist16, 2022-03-24
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	multiset<unsigned int> timeSet;
+	int N;
+	unsigned int totaltime = 0;
+	unsigned int currenttime = 0;
+
+	//입출력 속도 증가
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		unsigned int temp = 0;
+		cin >> temp;
+		timeSet.insert(temp);
+	}
+
+	for (auto i : timeSet)
+	{
+		currenttime += i;
+		totaltime += currenttime;
+	}
+
+	cout << totaltime;
+
+}
