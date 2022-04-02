7629cd44ce9a51fa7b9c8413dfa7b017341e1070
mistsixteen <mist.sixteen@gmail.com>
2022-04-03 08:17:54 +0900

    1158, 11866번 문제 풀이 업로드(요세푸스 문제)

---
+++ Backjoon/Problem_001158_요세푸스문제.cpp
@@ -0,0 +1,50 @@
+﻿//Backjoon Problem No.1158
+//https://www.acmicpc.net/problem/1158
+//Mist16, 2022-04-03
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int N, K;
+	int idx = 1;
+	queue<int> yQueue;
+
+	//입출력 속도 증가
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++)
+	{
+		yQueue.push(i);
+	}
+	
+	cout << "<";
+
+	while (yQueue.size() > 1)
+	{
+		int top = yQueue.front();
+		yQueue.pop();
+
+		if (idx == K)
+		{
+			cout << top << ", ";
+			idx = 1;
+		}
+		else
+		{
+			yQueue.push(top);
+			idx++;
+		}
+	}
+
+	cout << yQueue.front() << ">";
+
+	return 0;
+}
