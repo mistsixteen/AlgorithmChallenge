3dfd59b3bb6f7782cee66775ae5487429b84de6d
mistsixteen <mist.sixteen@gmail.com>
2022-04-08 11:23:49 +0900

    1427번 문제 풀이 업로드

---
+++ Backjoon/Problem_001427_소트인사이드.cpp
@@ -0,0 +1,27 @@
+﻿//Backjoon Problem No.1427
+//https://www.acmicpc.net/problem/1427
+//Mist16, 2022-04-08
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int arr[10] = { 0, };
+	string s;
+	cin >> s;
+
+	for (char c : s)
+	{
+		arr[c - '0']++;
+	}
+
+	for (int i = 9; i >= 0; i--)
+	{
+		for (int j = 0; j < arr[i]; j++)
+			cout << i;
+	}
+
+	return 0;
+}
