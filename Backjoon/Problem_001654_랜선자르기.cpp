dbcd6d455339e4caec35af1d90ca8163727d298d
mistsixteen <mist.sixteen@gmail.com>
2022-04-09 18:22:20 +0900

    1654, 2805번 문제 풀이 업로드

---
+++ Backjoon/Problem_001654_랜선자르기.cpp
@@ -0,0 +1,76 @@
+﻿//Backjoon Problem No.1654
+//https://www.acmicpc.net/problem/1654
+//Mist16, 2022-04-09
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<unsigned long> lines;
+int N, K;
+
+int run(unsigned long start, unsigned long end)
+{
+	unsigned long current = 0;
+	unsigned long middle = (start + end) / 2;
+
+	if (start + 1 == end)
+		middle = end;
+	if (start == end)
+		return start;
+
+	for (unsigned long i : lines)
+	{
+		if (i >= middle)
+		{
+			current += (i / middle);
+
+			if (current >= N)
+				break;
+		}
+	}
+
+	if (start + 1 == end)
+	{
+		if (N <= current)
+		{
+			return end;
+		}
+		else
+		{
+			return start;
+		}
+	}
+	
+	if (N <= current)
+		return run(middle, end);
+	else
+		return run(start, middle);
+}
+
+int main()
+{
+	unsigned long max = 0;
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> K >> N;
+	lines.resize(K);
+
+	for (auto iter = lines.begin(); iter != lines.end(); iter++)
+	{
+		cin >> *iter;
+		if (*iter >= N)
+			max += (*iter / N) + 1;
+		else
+			max += 1;
+	}
+	if (max == 0)
+		max = K;
+	cout << run(1, max);
+
+	return 0;
+}
---
+++ Backjoon/Problem_002805_나무자르기.cpp
@@ -0,0 +1,74 @@
+﻿//Backjoon Problem No.2805
+//https://www.acmicpc.net/problem/2805
+//Mist16, 2022-04-09
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+list<int> woods;
+int N;
+unsigned long long M;
+
+int run(int start, int end)
+{
+	unsigned long long current = 0;
+	int middle = (start + end) / 2;
+	if (start + 1 == end)
+		middle = end;
+
+	if (start == end)
+		return start;
+
+	for (int i : woods)
+	{
+		if (i > middle)
+		{
+			current += (i - middle);
+
+			if (current >= M)
+				break;
+		}
+	}
+
+	if (start + 1 == end)
+	{
+		if (M <= current)
+		{
+			return end;
+		}
+		else
+		{
+			return start;
+		}
+	}
+	
+	if (M <= current)
+		return run(middle, end);
+	else
+		return run(start, middle);
+}
+
+int main()
+{
+	int temp, max;
+	max = 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	woods.resize(N);
+
+	for (auto iter = woods.begin(); iter != woods.end(); iter++)
+	{
+		cin >> *iter;
+		if (*iter > max)
+			max = *iter;
+	}
+
+	cout << run(0, max);
+		
+
+	return 0;
+}
