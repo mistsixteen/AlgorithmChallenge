959303e28fbff0485a518d45a4fae30da0a28e89
mistsixteen <mist.sixteen@gmail.com>
2022-04-16 21:36:14 +0900

    1904번, 9184번 문제 풀이 업로드

---
+++ Backjoon/Problem_001904_01타일.cpp
@@ -0,0 +1,38 @@
+﻿//Backjoon Problem No.1904
+//https://www.acmicpc.net/problem/1904
+//Mist16, 2022-04-16
+
+#include <iostream>
+
+using namespace std;
+
+int memoArray[1000000] = { 0, };
+
+int run(int i)
+{
+	if (i < 0)
+		return 0;
+	if (memoArray[i] != 0)
+		return memoArray[i];
+	else
+	{
+		int temp = run(i - 1) + run(i - 2);
+		temp = temp % 15746;
+		memoArray[i] = temp;
+		return memoArray[i];
+	}
+
+}
+
+int main()
+{
+	int N;
+	memoArray[0] = 1;
+	memoArray[1] = 1;
+
+	cin >> N;
+
+	cout << run(N);
+
+	return 0;
+}
---
+++ Backjoon/Problem_009184_신나는함수실행.cpp
@@ -0,0 +1,47 @@
+﻿//Backjoon Problem No.9184﻿
+//https://www.acmicpc.net/problem/9184
+//Mist16, 2022-04-15
+
+#include <iostream>
+
+using namespace std;
+
+int memoryArray[51][51][51] = { 0, };
+
+
+int run(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+	if (memoryArray[a][b][c] != 0)
+		return memoryArray[a][b][c];
+
+	if (a > 20 || b > 20 || c > 20)
+	{
+		memoryArray[a][b][c] = run(20, 20, 20);
+	}
+	else if (a < b && b < c)
+	{
+		memoryArray[a][b][c] = run(a, b, c - 1) + run(a, b - 1, c - 1) - run(a, b - 1, c);
+	}
+	else
+	{
+		memoryArray[a][b][c] = run(a - 1, b, c) + run(a - 1, b - 1, c) + run(a - 1, b, c - 1) - run(a - 1, b - 1, c - 1);
+	}
+	return memoryArray[a][b][c];
+}
+
+int main()
+{
+	int a, b, c;
+
+	while (1)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+			break;
+		else
+			cout << "w(" << a << ", " << b << ", " << c << ") = " << run(a, b, c) << "\n";
+	}
+	return 0;
+}
