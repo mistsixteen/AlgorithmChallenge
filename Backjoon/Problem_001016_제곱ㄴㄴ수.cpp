b9ba700003b73af4666546aba1bcd4adb0bb7d01
mistsixteen <mist.sixteen@gmail.com>
2022-04-19 06:24:08 +0900

    16139, 1016번 문제 풀이 업로드

---
+++ Backjoon/Problem_001016_제곱ㄴㄴ수.cpp
@@ -0,0 +1,55 @@
+﻿//Backjoon Problem No.1016
+//https://www.acmicpc.net/problem/1016
+//Mist16, 2022-04-19
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int doubleArray[1000001] = { 0, };
+int main()
+{
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long idx = 2;
+	unsigned long long didx;
+	unsigned long long gap = 0;
+	int count = 0;
+	cin >> min >> max;
+	gap = max - min;
+	didx = idx * idx;
+
+	while (didx <= max)
+	{
+		unsigned long long current;
+
+		if (didx >= min)
+			current = didx;
+		else
+		{
+			unsigned long long temp = min / (idx * idx);
+			current = temp * didx;
+		}
+
+		while (current <= max)
+		{
+			if (current >= min)
+				doubleArray[current - min] = 1;
+			current += didx;
+		}
+
+		idx++;
+		didx = idx * idx;
+	}
+
+	for (int i = 0; i <= gap; i++)
+	{
+		if (doubleArray[i] == 0)
+		{
+			count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
---
+++ Backjoon/Problem_016139_인간컴퓨터상호작용.cpp
@@ -0,0 +1,41 @@
+﻿//Backjoon Problem No.16139
+//https://www.acmicpc.net/problem/16139
+//Mist16, 2022-04-18
+
+#include <iostream>
+
+using namespace std;
+
+int totalArray[200001][26] = { 0, };
+int main()
+{
+	string s;
+	int N;
+	char c;
+	int a, r;
+	int idx = 0;
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> s;
+
+	for (char c : s)
+	{
+		for (int i = 0; i < 26; i++)
+			totalArray[idx + 1][i] = totalArray[idx][i];
+		totalArray[idx + 1][(int)c - 'a']++;
+		idx++;
+	}
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> c >> a >> r;
+		cout << totalArray[r+1][(int)c - 'a'] - totalArray[a][(int)c - 'a'] << "\n";
+	}
+
+	return 0;
+}
