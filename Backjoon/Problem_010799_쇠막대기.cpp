25d070475cf148296ed6edb15537e13b0a41bb57
mistsixteen <mist.sixteen@gmail.com>
2022-04-24 04:20:26 +0900

    Create Problem_010799_쇠막대기.cpp

---
+++ Backjoon/Problem_010799_쇠막대기.cpp
@@ -0,0 +1,40 @@
+﻿//Backjoon Problem No.10799
+//https://www.acmicpc.net/problem/10799
+//Mist16, 2022-04-23
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	char lastChar = ' ';
+	int currentLevel = 0;
+	int totalPiece = 0;
+
+	cin >> str;
+
+	for (char c : str)
+	{
+		if (c == '(')
+		{
+			currentLevel++;
+		}
+		else if (c == ')')
+		{
+			currentLevel--;
+			if (lastChar == '(')
+			{
+				totalPiece += currentLevel;
+			}
+			else
+				totalPiece++;
+		}
+		lastChar = c;
+	}
+
+	cout << totalPiece;
+
+	return 0;
+}
