900b787ef09fdfe666f87f30277606ea39d7b672
mistsixteen <mist.sixteen@gmail.com>
2022-04-26 16:17:38 +0900

    Create Problem_002960_에라토스테네스의체.cpp

---
+++ Backjoon/Problem_002960_에라토스테네스의체.cpp
@@ -0,0 +1,39 @@
+﻿//Backjoon Problem No.2960
+//https://www.acmicpc.net/problem/2960
+//Mist16, 2022-04-26
+
+#include <iostream>
+using namespace std;
+
+int run(int N, int K)
+{
+	int numArray[1001] = { 0, };
+	int idx = 2;
+	while (idx <= N)
+	{
+		if (numArray[idx] == 0)
+		{
+			for (int i = idx; i <= N; i += idx)
+			{
+				if (numArray[i] == 0)
+				{
+					numArray[i] = 1;
+					K--;
+					if (K == 0)
+						return i;
+				}
+			}
+		}
+		idx++;
+	}
+
+}
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	cout << run(N, K);
+
+	return 0;
+}
