4b1a2d493d4304b7f1ae52a9e01bc4855f3e8d5d
mistsixteen <mist.sixteen@gmail.com>
2022-05-02 06:00:53 +0900

    Create Problem_006603_로또.cpp

---
+++ Backjoon/Problem_006603_로또.cpp
@@ -0,0 +1,55 @@
+﻿//Backjoon Problem No.6603
+//https://www.acmicpc.net/problem/6603
+//Mist16, 2022-05-01
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int k;
+int arr[30] = { 0, };
+
+void run(vector<int> fool, int idx, int left)
+{
+	if (idx >= k)
+		return;
+	if (k - idx == left)
+	{
+		for(int i = idx; i< k; i++)
+		{
+			fool.push_back(arr[i]);
+			left--;
+		}
+	}
+	if(left == 0)
+	{
+		for (int i : fool)
+		{
+			cout << i <<" ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	fool.push_back(arr[idx]);
+	run(fool, idx + 1, left - 1);
+	fool.pop_back();
+	run(fool, idx + 1, left);
+}
+
+int main()
+{
+	vector<int> blank;
+	while (true)
+	{
+		cin >> k;
+		if (k == 0)
+			break;
+		for (int i = 0; i < k; i++)
+			cin >> arr[i];
+		run(blank, 0, 6);
+		cout << "\n";
+	}
+
+	return 0;
+}
