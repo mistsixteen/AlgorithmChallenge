43a4254db09fbc6dbea6917be1a43becdccd59f4
mistsixteen <mist.sixteen@gmail.com>
2022-05-13 21:08:36 +0900

    Create Problem_016170_오늘의날짜는.cpp

---
+++ Backjoon/Problem_016170_오늘의날짜는.cpp
@@ -0,0 +1,26 @@
+﻿//Backjoon Problem No.16170
+//https://www.acmicpc.net/problem/16170
+//Mist16, 2022-05-13
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	time_t curTime = time(0);
+
+	tm* ptm = localtime(&curTime);
+	cout << 1900 + ptm->tm_year << "\n";
+	cout.width(2);
+	cout.fill('0');
+	cout << 1 + ptm->tm_mon << "\n";
+	cout.width(2);
+	cout.fill('0');
+	cout << ptm->tm_mday;
+	return 0;
+}
