966f7d90851548644087e6153808d4cc7dcb72f7
mistsixteen <mist.sixteen@gmail.com>
2022-05-17 23:36:29 +0900

    Create Problem_005543_상근날드.cpp

---
+++ Backjoon/Problem_005543_상근날드.cpp
@@ -0,0 +1,31 @@
+﻿//Backjoon Problem No.5543
+//https://www.acmicpc.net/problem/5543
+//Mist16, 2022-05-17
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int burger = 2000;
+	int drink = 2000;
+	int temp = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> temp;
+		if (burger > temp)
+			burger = temp;
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		cin >> temp;
+		if (drink > temp)
+			drink = temp;
+	}
+
+	cout << (burger + drink - 50);
+
+	return 0;
+}
