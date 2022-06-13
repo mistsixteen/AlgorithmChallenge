118a53d0d12c745d4b8f695ce204228a8ddd7a2c
mistsixteen <mist.sixteen@gmail.com>
2022-06-13 17:15:25 +0900

    Create Problem_023037_5의수난.cs

---
+++ Backjoon/Problem_023037_5의수난.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            string str;
+            str = Console.ReadLine();
+            for(int i = 0; i < 5; i++)
+            {
+                int temp = str[i] - '0';
+                result += (temp * temp * temp * temp * temp);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
