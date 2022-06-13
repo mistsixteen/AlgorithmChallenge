cacb6a725c34359813bec0a585994755e92f0fd8
mistsixteen <mist.sixteen@gmail.com>
2022-06-13 17:11:37 +0900

    Create Problem_002754_학점계산.cs

---
+++ Backjoon/Problem_002754_학점계산.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            float score;
+            str = Console.ReadLine();
+
+            switch (str[0])
+            {
+                case 'A':
+                    score = 4.0f;
+                    break;
+                case 'B':
+                    score = 3.0f;
+                    break;
+                case 'C':
+                    score = 2.0f;
+                    break;
+                case 'D':
+                    score = 1.0f;
+                    break;
+                default:
+                    score = 0.0f;
+                    break;
+            }
+            if (str.Length > 1)
+            {
+                if (str[1] == '+')
+                    score += 0.3f;
+                else if (str[1] == '-')
+                    score -= 0.3f;
+            }
+
+
+            Console.WriteLine("{0:0.0}", score);
+        }
+    }
+}
