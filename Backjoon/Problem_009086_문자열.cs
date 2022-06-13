2e40d27893f7af877be9baf6b6f4397b7649a8af
mistsixteen <mist.sixteen@gmail.com>
2022-06-13 17:11:40 +0900

    Create Problem_009086_문자열.cs

---
+++ Backjoon/Problem_009086_문자열.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases;
+            string str;
+            str = Console.ReadLine();
+            try
+            {
+                testCases = Int32.Parse(str);
+            }
+            catch (FormatException)
+            {
+                testCases = 0;
+            }
+            for (int i = 0; i < testCases; i++)
+            {
+                str = Console.ReadLine();
+                Console.WriteLine("" + str[0] + str[str.Length - 1]);
+            }
+        }
+    }
+}
