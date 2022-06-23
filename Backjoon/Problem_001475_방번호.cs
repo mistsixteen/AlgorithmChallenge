9a489870073b6cfe1e03628e634b940e9517f58b
mistsixteen <mist.sixteen@gmail.com>
2022-06-23 22:32:48 +0900

    Create Problem_001475_방번호.cs

---
+++ Backjoon/Problem_001475_방번호.cs
@@ -0,0 +1,51 @@
+﻿//Backjoon Problem No.1475
+//https://www.acmicpc.net/problem/1475
+//Mist16, 2022-06-23
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            int[] array = new int[10];
+            int max = 0;
+
+            foreach(char c in str)
+            {
+                switch(c)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '7':
+                    case '8':
+                        array[(c - '0')]++;
+                        break;
+                    case '6':
+                    case '9':
+                        array[6]++;
+                        break;
+                }
+            }
+            if (array[6] % 2 == 1)
+                array[6]++;
+
+            array[6] = array[6] / 2;
+
+            for(int i = 0; i < 9; i++)
+            {
+                if (max < array[i])
+                    max = array[i];
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
