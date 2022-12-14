d928bbdad22558ea84c1a36133b2d575b70778de
mistsixteen <mist.sixteen@gmail.com>
2022-12-14 14:03:41 +0900

    C# : 14581번 팬들에게 둘러싸인 홍준 문제 업로드

---
+++ Backjoon/Bronze 5/Problem_014581_팬들에게둘러싸인홍준.cs
@@ -0,0 +1,22 @@
+﻿//Backjoon Problem No.14581
+//https://www.acmicpc.net/problem/14581
+//Mist16, 2022-12-14
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine();
+
+            Console.WriteLine(":fan::fan::fan:");
+            Console.WriteLine(":fan::" + input + "::fan:");
+            Console.WriteLine(":fan::fan::fan:");
+        }
+    }
+}
