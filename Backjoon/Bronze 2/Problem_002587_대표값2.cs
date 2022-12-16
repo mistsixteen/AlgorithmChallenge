3a494d964da000fb81b9180abceb45e20c4fcc49
mistsixteen <mist.sixteen@gmail.com>
2022-12-16 14:40:09 +0900

    C# : 2587번 대표값2 문제 업로드

---
+++ Backjoon/Bronze 2/Problem_002587_대표값2.cs
@@ -0,0 +1,29 @@
+﻿//Backjoon Problem No.2587
+//https://www.acmicpc.net/problem/2587
+//Mist16, 2022-12-16
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int total = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                var str = Console.ReadLine();
+                int input = int.Parse(str);
+                list.Add(input);
+                total += input;
+            }
+            list.Sort();
+            Console.WriteLine((int)total/5);
+            Console.WriteLine(list[2]);
+        }
+    }
+}
