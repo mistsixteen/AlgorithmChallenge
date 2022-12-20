20d60a0bde5f54a84b41c80028abd267eb75ebb0
mistsixteen <mist.sixteen@gmail.com>
2022-12-20 14:31:19 +0900

    C# : 25305번 커트라인 문제 풀이 업로드

---
+++ Backjoon/Bronze 2/Problem_025305_커트라인.cs
@@ -0,0 +1,33 @@
+﻿//Backjoon Problem No.25305
+//https://www.acmicpc.net/problem/25305
+//Mist16, 2022-12-20
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int n, k;
+            var input = Console.ReadLine();
+            var words = input.Split(' ');
+            n = int.Parse(words[0]);
+            k = int.Parse(words[1]);
+
+            input = Console.ReadLine();
+            words = input.Split(" ");
+
+            for (int i = 0; i < n; i++)
+            {
+                var score = int.Parse(words[i]);
+                list.Add(score);
+            }
+            list.Sort();
+            Console.WriteLine(list[n - k]);
+        }
+    }
+}
