66234e32bd3ab29be3edce8eccaf80480ca6865f
mistsixteen <mist.sixteen@gmail.com>
2022-12-22 19:54:47 +0900

    C++ : 3273번 두 수의 합 문제 업로드

---
+++ Backjoon/Silver 3/Problem_003273_두 수의 합.cpp
@@ -0,0 +1,54 @@
+﻿//Backjoon Problem No.3273
+//https://www.acmicpc.net/problem/3273
+//Mist16, 2022-12-22
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int> array;
+	int n, x;
+
+	cin >> n;
+	array.reserve(n);
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		cin >> temp;
+		array.push_back(temp);
+	}
+	cin >> x;
+
+	sort(array.begin(), array.end());
+
+	if (n == 1)
+	{
+		cout << "0";
+		return 0;
+	}
+
+	int posa = 0;
+	int posb = n - 1;
+	int answer = 0;
+
+	while (posa < posb)
+	{
+		int temp = array[posa] + array[posb];
+		if (temp == x)
+		{
+			answer++;
+			posb--;
+		}
+		else if (temp > x)
+			posb--;
+		else
+			posa++;
+	}
+
+	cout << answer << endl;
+
+	return 0;
+}
