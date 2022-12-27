05e89782cc7103dcfa4dea3a566f04981ee1711f
mistsixteen <mist.sixteen@gmail.com>
2022-12-27 12:08:59 +0900

    C++ : 14425번 문자열 집합 문제 풀이 업로드

---
+++ Backjoon/Silver 3/Problem_014425_문자열집합.cpp
@@ -0,0 +1,34 @@
+﻿//Backjoon Problem No.14425
+//https://www.acmicpc.net/problem/14425
+//Mist16, 2022-12-27
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	int answer = 0;
+	string str;
+	set<string> stringset;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str;
+		stringset.insert(str);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> str;
+		if (stringset.find(str) != stringset.end())
+		{
+			answer++;
+		}
+	}
+
+	cout << answer;
+
+}
