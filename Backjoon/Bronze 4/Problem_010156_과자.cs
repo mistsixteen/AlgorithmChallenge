2913b1a203aa1450f11bb0c376a0b36636023a43
mistsixteen <mist.sixteen@gmail.com>
2022-12-31 23:58:53 +0900

    C# : 10156번 과자 문제 풀이 업로드

---
+++ Backjoon/Bronze 4/Problem_010156_과자.cs
@@ -0,0 +1,25 @@
+﻿//Backjoon Problem No.10156
+//https://www.acmicpc.net/problem/10156
+//Mist16, 2022-12-30
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] words = str.Split(' ');
+            int price = int.Parse(words[0]);
+            int count = int.Parse(words[1]);
+            int currentmoney = int.Parse(words[2]);
+
+            var moneyRequire = (price * count) - currentmoney;
+            if (moneyRequire < 0) moneyRequire = 0;
+
+            Console.WriteLine(moneyRequire);
+        }
+    }
+}
