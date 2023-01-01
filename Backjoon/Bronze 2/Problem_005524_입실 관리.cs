525c5c7f12cfd36fac2cb53a23a48d0b451e0cec
mistsixteen <mist.sixteen@gmail.com>
2023-01-01 22:48:17 +0900

    C# : 5524번 입실 관리 문제 풀이 업로드

---
+++ Backjoon/Bronze 2/Problem_005524_입실 관리.cs
@@ -0,0 +1,24 @@
+﻿//Backjoon Problem No.5524
+//https://www.acmicpc.net/problem/5524
+//Mist16, 2022-01-01
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int testCases = int.Parse(str);
+
+            for (int i = 0; i < testCases; i++)
+            {
+                str = Console.ReadLine();
+                Console.WriteLine(str.ToLower());
+            }
+
+        }
+    }
+}
