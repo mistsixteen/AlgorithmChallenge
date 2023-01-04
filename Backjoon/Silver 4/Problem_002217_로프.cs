82539940bc62fdcdd58d788b06c4d91168abc245
mistsixteen <mist.sixteen@gmail.com>
2023-01-04 21:11:33 +0900

    C# : 2217번 로프 문제 풀이 업로드

---
+++ Backjoon/Silver 4/Problem_002217_로프.cs
@@ -0,0 +1,36 @@
+﻿//Backjoon Problem No.2217
+//https://www.acmicpc.net/problem/2217
+//Mist16, 2022-01-04
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(str);
+
+            List<int> li = new List<int>(n);
+            for (int i = 0; i < n; i++)
+            {
+                str = Console.ReadLine();
+                var number = int.Parse(str);
+                li.Add(number);
+            }
+            li.Sort();
+
+            //run
+            long max = 0;
+            for (int i = 1; i <= li.Count; i++)
+            {
+                long newvalue = li[n - i] * i;
+                if (newvalue > max) max = newvalue;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
