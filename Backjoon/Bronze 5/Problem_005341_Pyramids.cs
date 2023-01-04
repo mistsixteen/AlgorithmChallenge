e82e9ca9392e520abae59f09f48a7d8d69b1a6cc
mistsixteen <mist.sixteen@gmail.com>
2023-01-04 21:11:42 +0900

    B5 문제들 업로드

---
+++ Backjoon/Bronze 5/Problem_005341_Pyramids.cs
@@ -0,0 +1,23 @@
+﻿//Backjoon Problem No.5341
+//https://www.acmicpc.net/problem/5341
+//Mist16, 2022-01-04
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+                int n = int.Parse(str);
+                if (n == 0) break;
+                int result = (n) * (n + 1) / 2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Backjoon/Bronze 5/Problem_006840_WhoIsInTheMiddle.cs
@@ -0,0 +1,23 @@
+﻿//Backjoon Problem No.6840
+//https://www.acmicpc.net/problem/6840
+//Mist16, 2022-01-04
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+                int n = int.Parse(str);
+                if (n == 0) break;
+                int result = (n) * (n + 1) / 2;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
