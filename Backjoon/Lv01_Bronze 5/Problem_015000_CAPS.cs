378e467e08d75a65e88738adac709952697d9f1d
임병욱 / Mist16 <mist.sixteen@gmail.com>
2025-06-06 23:27:29 +0900

    Create Problem_015000_CAPS.cs

---
+++ Backjoon/Lv01_Bronze 5/Problem_015000_CAPS.cs
@@ -0,0 +1,21 @@
+//Backjoon Problem No.15000
+//https://www.acmicpc.net/problem/15000
+//Mist16, 2025-06-06
+
+#nullable disable
+
+using System;
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            
+            //OutPut
+            Console.WriteLine(str.ToUpper());
+        }
+    }
+}
