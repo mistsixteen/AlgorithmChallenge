5e906457e81eeb7f89b4d20746edabdac507d03b
임병욱 / Mist16 <mist.sixteen@gmail.com>
2025-06-11 23:53:30 +0900

    Create Problem_032951_AI선도대학.cs

---
+++ Backjoon/Lv01_Bronze 5/Problem_032951_AI선도대학.cs
@@ -0,0 +1,20 @@
+﻿//Backjoon Problem No.32951
+//https://www.acmicpc.net/problem/32951
+//Mist16, 2025-06-11
+
+#nullable disable
+
+namespace CSharp_Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            var year = int.Parse(str);
+
+            Console.WriteLine(year - 2024);
+        }
+    }
+}
