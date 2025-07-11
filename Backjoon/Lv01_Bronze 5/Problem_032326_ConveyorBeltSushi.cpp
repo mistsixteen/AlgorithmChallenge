f703dfd892b7bb3ccc28134b9b381357cd5dce9a
임병욱 / Mist16 <mist.sixteen@gmail.com>
2025-07-11 23:41:25 +0900

    Create Problem_032326_ConveyorBeltSushi.cpp

---
+++ Backjoon/Lv01_Bronze 5/Problem_032326_ConveyorBeltSushi.cpp
@@ -0,0 +1,22 @@
+//Backjoon Problem No.32326
+//https://www.acmicpc.net/problem/32326
+//Mist16, 2025-07-11
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * 3 + b * 4 + c * 5;
+
+    return 0;
+}
